7aa428891922b3d643e77e69143a23d2c5b82fb8
kanatamon <ponde.moonlord@gmail.com>
2017-09-04 23:58:27 +0700

    Add ability to update state with increment

--- src/state.js
+++ src/state.js
@@ -1,9 +1,13 @@
+import { INCREMENT } from './actions';
+
 export function init() {
   return { counter: 0 };
 }
 
 export function update(action, state) {
   switch (action) {
+    case INCREMENT:
+      return { ...state, counter: state.counter + 1 };
     default:
       return { ...state };
   }
--- src/state.spec.js
+++ src/state.spec.js
@@ -1,4 +1,5 @@
 import * as State from './state';
+import * as action from './actions';
 
 describe('Counter state', () => {
   describe('#init state', () => {
@@ -17,5 +18,10 @@ describe('Counter state', () => {
       const state = State.init();
       expect(State.update('NOSENSE_ACTION', state)).not.toBe(state);
     });
+
+    it('should increase counter value from 0 => 1', () => {
+      const state = State.init();
+      expect(State.update(action.INCREMENT, state)).toEqual({ counter: 1 });
+    })
   });
 });
